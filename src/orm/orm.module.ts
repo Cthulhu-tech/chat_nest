b8959174582d47125817b030e9c025ebc17618b2
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2024-02-28 14:35:51 +0400

    orm

---
+++ src/orm/orm.module.ts
@@ -0,0 +1,21 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Chat } from 'src/chat/entities/chat.entity';
+import { Message } from 'src/messages/entities/message.entity';
+import { Token } from 'src/token/entities/token.entity';
+import { User } from 'src/user/entities/user.entity';
+
+export const OrmModule = TypeOrmModule.forRootAsync({
+  inject: [ConfigService],
+  useFactory: (config) => ({
+    type: config.get('TYPE') as any,
+    host: config.get('HOST'),
+    port: Number.parseInt(config.get('PORT')),
+    username: config.get('USERNAME'),
+    password: config.get('PASSWORD'),
+    database: config.get('DATABASE'),
+    entities: [User, Token, Chat, Message],
+    synchronize: true,
+    dropSchema: true,
+  }),
+});
