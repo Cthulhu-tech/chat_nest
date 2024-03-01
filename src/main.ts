bea40e682f87ff2cd1defd70d2cd4548a0158cc4
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2024-03-01 16:07:24 +0400

    update env and docker-compose.yml

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+		origin: process.env.CORS_HOST,
+		methods: "GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS",
+		credentials: true,
+		optionsSuccessStatus: 204, 
+		allowedHeaders:
+			"Authorization, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
+	});
   app.use(cookieParser());
   await app.listen(3000);
 }
