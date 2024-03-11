69d5ef43f5d749b8ae47837a89da1434cd53c426
Ratctangle <90329246+Cthulhu-tech@users.noreply.github.com>
2024-03-11 17:51:58 +0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -13,9 +13,6 @@ async function bootstrap() {
 			"Authorization, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
 	});
   app.use(cookieParser());
-  await app.listen(3000, '0.0.0.0.', void function (err: Error) {
-    if (err) throw err
-    console.log(`Listening on port ${3000}`);
-  })
+  await app.listen(3000);
 }
 bootstrap();
